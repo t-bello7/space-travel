a009e1ecf665051ee14cb7628f72c3ade365c022
t-bello7 <bello4aus@gmail.com>
2022-03-27 17:20:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('.primary-navigation')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+
+
+// when someone clicks the hamburger button
+navToggle.addEventListener("click", ()=>{
+// if the nav is closed, open it
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility === "false"){
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else{
+        nav.setAttribute("data-visible", false)
+        navToggle.setAttribute("aria-expanded", false);
+    }
+// ifthe nav is open , close it
+})
